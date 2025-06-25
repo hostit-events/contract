f1f86de0bcf33b0bae8fce82ae29904282ca08dd
David Dada <daveproxy80@gmail.com>
2025-06-25 09:27:19 +0100

    chore: add base and lisk mainnet and sepolia addresses

---
+++ src/libraries/constants/TokenAddresses.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+//*//////////////////////////////////////////////////////////////////////////
+//                           BASE SEPOLIA ADDRESSES
+//////////////////////////////////////////////////////////////////////////*//
+
+address constant BASE_SEPOLIA_USDC_ADDRESS = 0x036CbD53842c5426634e7929541eC2318f3dCF7e;
+address constant BASE_SEPOLIA_EURC_ADDRESS = 0x808456652fdb597867f38412077A9182bf77359F;
+address constant BASE_SEPOLIA_USDT_ADDRESS = 0x7169D38820dfd117C3FA1f22a697dBA58d90BA06;
+
+//*//////////////////////////////////////////////////////////////////////////
+//                           LISK SEPOLIA ADDRESSES
+//////////////////////////////////////////////////////////////////////////*//
+
+address constant LISK_SEPOLIA_USDC_ADDRESS = 0x0E82fDDAd51cc3ac12b69761C45bBCB9A2Bf3C83;
+address constant LISK_SEPOLIA_LSK_ADDRESS = 0x8a21CF9Ba08Ae709D64Cb25AfAA951183EC9FF6D;
+
+//*//////////////////////////////////////////////////////////////////////////
+//                               BASE ADDRESSES
+//////////////////////////////////////////////////////////////////////////*//
+
+address constant BASE_USDC_ADDRESS = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
+address constant BASE_EURC_ADDRESS = 0x60a3E35Cc302bFA44Cb288Bc5a4F316Fdb1adb42;
+address constant BASE_USDT_ADDRESS = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
+
+//*//////////////////////////////////////////////////////////////////////////
+//                               LISK ADDRESSES
+//////////////////////////////////////////////////////////////////////////*//
+
+address constant LISK_USDC_ADDRESS = 0xF242275d3a6527d877f2c927a82D9b057609cc71;
+address constant LISK_EURC_ADDRESS = 0xe12cEFaAD61e551691BFa5cDA36e5dE051778C65;
+address constant LISK_LSK_ADDRESS = 0x8a21CF9Ba08Ae709D64Cb25AfAA951183EC9FF6D;
+address constant LISK_USDT_ADDRESS = 0x05D032ac25d322df992303dCa074EE7392C117b9;
+address constant LISK_USDT0_ADDRESS = 0x43F2376D5D03553aE72F4A8093bbe9de4336EB08;
